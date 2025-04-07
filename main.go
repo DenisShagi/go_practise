ae8aa7e17e789073e365fbe8cb77eeabf07c1a6f
DenisShag <denis.shagim@icloud.com>
2025-04-07 22:32:02 +0500

    feat: create function for converter

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func main() {
+	amountUSD := 1.0
+	exchangeRateEUR := 0.9     
+	exchangeRateRUB := 85.0    
+
+	amountEUR := amountUSD * exchangeRateEUR
+	amountRUB := amountUSD * exchangeRateRUB
+	amountEURinRUB := amountEUR / exchangeRateEUR * amountRUB
+
+	fmt.Printf("EUR in RUB: %.2f\n", amountEURinRUB)
 }
